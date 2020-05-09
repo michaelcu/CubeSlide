014ffcafdf9a4a7ed6ca16c6a00f9f875c0b67a8
Michael Cu <Michael.sy.cu@gmail.com>
2020-05-09 04:17:08 -0500

    Added File

---
+++ MyProject/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public Player_Movement movement;
+    
+
+    //Works as long as there's a rigidbody or collider
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        //Debug.Log("We hit something.");
+
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+         
+        }
+    }
+
+}
